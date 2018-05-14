48196b1b530f563e0e034363577c9a265657b400
guomaoqiu@sina.com <guomaoqiu@sina.com>
2018-05-14 18:21:13 +0800

    first commit

--- saltstack/files/zabbix_agent/scripts/redis-discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import json
-t=os.popen(""" ps -ef | grep redis-server | grep -v grep | awk  '{print $NF}' | awk -F: '{print $2}' """)
-ports = []
-for port in  t.readlines():
-        r = os.path.basename(port.strip())
-        ports += [{'{#REDISPORT}':r}]
-print json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':'))
