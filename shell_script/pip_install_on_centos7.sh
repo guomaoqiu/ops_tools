f2b1c31a4807a6b5f0ceeacfdfbd309cb141d953
guomaoqiu <guomaoqiu@sina.com>
2019-07-16 09:25:22 -0400

    add shell script

---
+++ shell_script/pip_install_on_centos7.sh
@@ -0,0 +1,6 @@
+# 用于centos安装pip组件
+yum install epel-release
+sudo yum install python-pip
+sudo yum install python-develsudo yum groupinstall 'development tools'
+sudo yum install python-devel ;sudo yum groupinstall 'development tools'
+pip install requests
