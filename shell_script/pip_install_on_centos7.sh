f46202c23d3384144c98187487c8928e8265300b
OutMan <2399447849@qq.com>
2019-07-17 17:52:06 +0800

    Update pip_install_on_centos7.sh

--- shell_script/pip_install_on_centos7.sh
+++ shell_script/pip_install_on_centos7.sh
@@ -1,6 +1,6 @@
 # 用于centos安装pip组件
-yum install epel-release
-sudo yum install python-pip
-sudo yum install python-develsudo yum groupinstall 'development tools'
-sudo yum install python-devel ;sudo yum groupinstall 'development tools'
+yum install epel-release -y
+sudo yum install python-pip -y
+sudo yum install python-develsudo yum groupinstall 'development tools' -y
+sudo yum install python-devel ;sudo yum groupinstall 'development tools' -y
 pip install requests
