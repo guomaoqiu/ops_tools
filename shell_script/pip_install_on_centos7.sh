bca63592f9747a03310af8386e20677ba4b0b23a
OutMan <2399447849@qq.com>
2019-07-17 18:00:23 +0800

    Update pip_install_on_centos7.sh

--- shell_script/pip_install_on_centos7.sh
+++ shell_script/pip_install_on_centos7.sh
@@ -2,5 +2,6 @@
 yum install epel-release -y
 yum install python-pip -y
 yum install python-develsudo yum groupinstall 'development tools' -y
-yum install python-devel ;sudo yum groupinstall 'development tools' -y
+yum install python-devel -y
+yum groupinstall 'development tools' -y
 pip install requests
